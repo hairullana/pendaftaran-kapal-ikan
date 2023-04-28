b43298adee922e81d315698e7c2bcea11c54a29b
Hairul Lana <hairullana99@gmail.com>
2023-04-28 13:44:45 +0800

    seeder create user and role

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,10 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Spatie\Permission\Models\Role;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,11 +17,26 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        // default akun admin
+        $admin = User::create([
+            'email' => 'admin@gmail.com',
+            'name' => 'admin',
+            'password' => Hash::make(123),
+            'email_verified_at' => now(),
+            'status' => User::DITERIMA,
+        ]);
+        $user = User::create([
+            'email' => 'user@gmail.com',
+            'name' => 'user',
+            'password' => Hash::make(123),
+            'email_verified_at' => now(),
+            'status' => User::DITERIMA,
+        ]);
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        Role::create(['name' => 'admin']);
+        Role::create(['name' => 'user']);
+
+        $admin->assignRole('admin');
+        $user->assignRole('user');
     }
 }
